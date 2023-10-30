cd0a51f28b4bf7c80e1106feb3f46ddbd078bc7e
Larry <tradesouthwest@outlook.com>
2023-10-30 09:41:35 -0700

    Add files via upload

---
+++ includes/starter-plugin-addin-functions.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * 
+ */
+
+/**
+ * Supported ob_end_flush() for all levels
+ *
+ * This replaces the WordPress `wp_ob_end_flush_all()` function
+ * with a replacement that doesn't cause PHP notices.
+ */
+remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
+add_action( 'shutdown', function() {
+   while ( @ob_end_flush() );
+} );
